4e99e0f7d3b012f92dfdfea9e24c5ea968fb970f
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-26 09:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+    function calculateTotal() {
+      var prices = document.querySelectorAll('[data-ns-test="prices"]');
+      var total = 0;
+
+      for (var i = 0; i < prices.length; i++) {
+        total += parseFloat(prices[i].textContent);
+      }
+
+      var table = document.getElementById('groceryTable');
+      var newRow = document.createElement('tr');
+      var newCell = document.createElement('td');
+
+      newCell.colSpan = 2;
+      newCell.textContent = 'Total Price: ' + total.toFixed(2);
+      newRow.appendChild(newCell);
+      table.appendChild(newRow);
+    }
+
+    calculateTotal();
+  
